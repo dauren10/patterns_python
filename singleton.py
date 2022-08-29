d4e671f17fc058f088d13563ec10949198f767c3
Dauren Shalabayev <d.shalabayev@bsl.dev>
2022-08-29 13:03:22 +0600

    Init

---
+++ singleton.py
@@ -0,0 +1,10 @@
+class Singleton:
+    _instance = None
+    def __new__(cls):
+        if not isinstance(cls._instance,cls):
+            cls._instance = super().__new__(cls)
+            return cls._instance
+
+s = Singleton()
+print(id(s))
+print(id(s))
