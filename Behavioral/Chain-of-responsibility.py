c70a7bce888234f800d3c23d66017510a772b77b
OEM Configuration (temporary user) <dauren.shalabayev@gmail.com>
2023-02-14 00:52:10 +0600

    ChainOfResponsibility, Command patterns

--- Behavioral/Chain-of-responsibility.py
+++ Behavioral/Chain-of-responsibility.py
@@ -1,1 +1,135 @@
-'''Предназначен для организации в системе уровней ответственности.'''
+'''Предназначен для организации в системе уровней ответственности.
+Метод цепочки ответственности — это поведенческий шаблон проектирования, 
+который является объектно-ориентированной версией if … elif … elif … else и позволяет нам 
+динамически переставлять блоки условий и действий во время выполнения. Это позволяет нам передавать запросы по цепочке обработчиков. 
+Обработка проста: всякий раз, когда какой-либо обработчик получает запрос, у него есть два варианта: либо обработать его, либо передать 
+следующему обработчику в цепочке.
+Этот шаблон направлен на то, чтобы отделить отправителей запроса от его получателей, позволяя запросу проходить через получателей 
+в цепочке, пока он не будет обработан.
+'''
+'''
+Представьте, что вы создаете простой веб-сайт, который принимает входные строки и сообщает о 
+различных свойствах строк, таких как строковый палиндром? строка в верхнем регистре? строка в 
+нижнем регистре? и многие другие свойства тоже. После полного планирования вы решаете, что эти 
+проверки входной строки должны выполняться последовательно. Итак, здесь возникает проблема для разработчика, 
+что он должен реализовать такое приложение, которое может решить во время выполнения, какое действие должно быть выполнено следующим.
+ 
+Решение с использованием метода цепочки ответственности
+
+Метод цепочки ответственности обеспечивает решение описанной выше проблемы. 
+Он создает отдельный абстрактный обработчик, который используется для обработки последовательных операций, 
+которые должны выполняться динамически. Например, мы создаем четыре обработчика с именами 
+'''
+'''geeksforgeeks.org'''
+
+class AbstractHandler(object):
+
+	"""Parent class of all concrete handlers"""
+
+	def __init__(self, nxt):
+
+		"""change or increase the local variable using nxt"""
+
+		self._nxt = nxt
+
+	def handle(self, request):
+
+		"""It calls the processRequest through given request"""
+
+		handled = self.processRequest(request)
+
+		"""case when it is not handled"""
+
+		if not handled:
+			self._nxt.handle(request)
+
+	def processRequest(self, request):
+
+		"""throws a NotImplementedError"""
+
+		raise NotImplementedError('First implement it !')
+
+
+class FirstConcreteHandler(AbstractHandler):
+
+	"""Concrete Handler # 1: Child class of AbstractHandler"""
+
+	def processRequest(self, request):
+
+		'''return True if request is handled '''
+
+		if 'a' < request <= 'e':
+			print("This is {} handling request '{}'".format(self.__class__.__name__, request))
+			return True
+
+
+class SecondConcreteHandler(AbstractHandler):
+
+	"""Concrete Handler # 2: Child class of AbstractHandler"""
+
+	def processRequest(self, request):
+
+		'''return True if the request is handled'''
+
+		if 'e' < request <= 'l':
+			print("This is {} handling request '{}'".format(self.__class__.__name__, request))
+			return True
+
+class ThirdConcreteHandler(AbstractHandler):
+
+	"""Concrete Handler # 3: Child class of AbstractHandler"""
+
+	def processRequest(self, request):
+
+		'''return True if the request is handled'''
+
+		if 'l' < request <= 'z':
+			print("This is {} handling request '{}'".format(self.__class__.__name__, request))
+			return True
+
+class DefaultHandler(AbstractHandler):
+
+	"""Default Handler: child class from AbstractHandler"""
+
+	def processRequest(self, request):
+
+		"""Gives the message that the request is not handled and returns true"""
+
+		print("This is {} telling you that request '{}' has no handler right now.".format(self.__class__.__name__,
+																						request))
+		return True
+
+
+class User:
+
+	"""User Class"""
+
+	def __init__(self):
+
+		"""Provides the sequence of handles for the users"""
+
+		initial = None
+
+		self.handler = FirstConcreteHandler(SecondConcreteHandler(ThirdConcreteHandler(DefaultHandler(initial))))
+
+	def agent(self, user_request):
+
+		"""Iterates over each request and sends them to specific handles"""
+
+		for request in user_request:
+			self.handler.handle(request)
+
+"""main method"""
+
+if __name__ == "__main__":
+
+	"""Create a client object"""
+	user = User()
+
+	"""Create requests to be processed"""
+
+	string = "GeeksforGeeks"
+	requests = list(string)
+
+	"""Send the requests one by one, to handlers as per the sequence of handlers defined in the Client class"""
+	user.agent(requests)
--- Behavioral/Command.py
+++ Behavioral/Command.py
@@ -1,1 +1,60 @@
 '''Представляет действие. Объект команды заключает в себе само действие и его параметры.'''
+'''
+Метод команды — это поведенческий шаблон проектирования, который инкапсулирует запрос в виде объекта, тем самым обеспечивая параметризацию клиентов с различными запросами, а также постановку в очередь или регистрацию запросов. Параметризация других объектов с другими запросами в нашей аналогии означает, что кнопка, используемая для включения света, может быть позже использована для включения стереосистемы или, возможно, для открытия двери гаража. Это помогает продвигать «вызов метода объекта» к полному статусу объекта. По сути, он инкапсулирует всю информацию, необходимую для выполнения действия или запуска события.
+Проблема без использования командного метода
+
+Представьте, что вы работаете над редактором кода. Ваша текущая задача — добавить новые кнопки на панель инструментов редактора для различных операций. Определенно легко создать один класс кнопок, который можно использовать для кнопок. Поскольку мы знаем, что все кнопки, используемые в редакторе, выглядят одинаково, так что же нам делать? Должны ли мы создавать множество подклассов для каждого места, где используется кнопка?
+'''
+
+"""Use built-in abc to implement Abstract classes and methods"""
+from abc import ABC, abstractmethod
+
+"""Class Dedicated to Command"""
+class Command(ABC):
+	
+	"""constructor method"""
+	def __init__(self, receiver):
+		self.receiver = receiver
+	
+	"""process method"""
+	def process(self):
+		pass
+
+"""Class dedicated to Command Implementation"""
+class CommandImplementation(Command):
+	
+	"""constructor method"""
+	def __init__(self, receiver):
+		self.receiver = receiver
+
+	"""process method"""
+	def process(self):
+		self.receiver.perform_action()
+
+"""Class dedicated to Receiver"""
+class Receiver:
+	
+	"""perform-action method"""
+	def perform_action(self):
+		print('Action performed in receiver.')
+
+"""Class dedicated to Invoker"""
+class Invoker:
+	
+	"""command method"""
+	def command(self, cmd):
+		self.cmd = cmd
+
+	"""execute method"""
+	def execute(self):
+		self.cmd.process()
+
+"""main method"""
+if __name__ == "__main__":
+	
+	"""create Receiver object"""
+	receiver = Receiver()
+	cmd = CommandImplementation(receiver)
+	invoker = Invoker()
+	invoker.command(cmd)
+	invoker.execute()
